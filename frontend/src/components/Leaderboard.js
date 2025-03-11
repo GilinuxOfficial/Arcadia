57c7a71a61353964ed64c4ad128260eed76a364c
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:58:25 -0400

    Update Leaderboard.js

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -1,1 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+const Leaderboard = () => {
+  const [leaders, setLeaders] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/leaderboard')
+      .then(res => setLeaders(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div className="leaderboard">
+      <h2>Leaderboard</h2>
+      <ul>
+        {leaders.map((leader, index) => (
+          <li key={index}>{leader.username} - Wins: {leader.wins}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Leaderboard;
