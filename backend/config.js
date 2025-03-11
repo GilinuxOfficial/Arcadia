5b364bd54f9ef16d25468ee8697a088807b97de7
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:49:02 -0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+  discord: {
+    clientID: process.env.DISCORD_CLIENT_ID,
+    clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    callbackURL: process.env.DISCORD_CALLBACK_URL
+  },
+  sessionSecret: process.env.SESSION_SECRET || 'supersecret',
+  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/deviantlounge'
+};
