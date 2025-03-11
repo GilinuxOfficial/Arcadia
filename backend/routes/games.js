90f3c56225e2de0ddf1fedcaf9dfd17c6f5db8c5
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:50:11 -0400

    Update games.js

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const gamesController = require('../controllers/gamesController');
 
+// Get list of available games
+router.get('/', gamesController.getGames);
+
+// Play a game (dummy logic)
+router.post('/play', gamesController.playGame);
+
+module.exports = router;
