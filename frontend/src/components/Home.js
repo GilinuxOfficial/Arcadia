ffa4ea87ce0492d4b094443f07221aba555da9b7
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:56:57 -0400

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Welcome to Deviant Lounge</h2>
+      <p>Experience an immersive arcade with dark, gothic aesthetics inspired by Dante’s Divine Comedy.</p>
+    </div>
+  );
+};
+
+export default Home;
