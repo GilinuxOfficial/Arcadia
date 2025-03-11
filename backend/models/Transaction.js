2ec192178cb7bfdcbc30dd9c25d24102dd2b588e
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:54:22 -0400

    Update Transaction.js

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const TransactionSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  type: { type: String, enum: ['purchase', 'bonus'], required: true },
+  amount: { type: Number, required: true },
+  currency: { type: String, enum: ['arcadianTokens', 'ducats'], required: true },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
