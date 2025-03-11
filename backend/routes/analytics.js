1dc4c348be6fb711f9d1eaf7309eb899213de03a
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:51:14 -0400

    Update analytics.js

--- backend/routes/analytics.js
+++ backend/routes/analytics.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const analyticsController = require('../controllers/analyticsController');
 
+// Get analytics data
+router.get('/', analyticsController.getAnalytics);
+
+module.exports = router;
