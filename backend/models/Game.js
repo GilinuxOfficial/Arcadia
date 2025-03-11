eb59ed63134c7f1deda569ec27c6c9f91be11190
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:54:40 -0400

    Update Game.js

--- backend/models/Game.js
+++ backend/models/Game.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const GameSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  stakes: { type: String }
+});
+
+module.exports = mongoose.model('Game', GameSchema);
