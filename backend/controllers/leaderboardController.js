e98a11e8ea90f57fb2835dadf10e0b01c7355e11
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:52:44 -0400

    Update leaderboardController.js

--- backend/controllers/leaderboardController.js
+++ backend/controllers/leaderboardController.js
@@ -1,1 +1,9 @@
-
+exports.getLeaderboard = (req, res) => {
+  // Dummy leaderboard data
+  const leaderboard = [
+    { username: 'Player1', wins: 10 },
+    { username: 'Player2', wins: 8 },
+    { username: 'Player3', wins: 5 }
+  ];
+  res.json(leaderboard);
+};
