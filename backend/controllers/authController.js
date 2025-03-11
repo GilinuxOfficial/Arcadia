fa260dc1b0fcf8de75cb661a84c027ecd8010c0c
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:51:39 -0400

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,39 @@
+const DiscordStrategy = require('passport-discord').Strategy;
+const User = require('../models/User');
+const config = require('../config');
 
+module.exports.initPassport = function(passport) {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+  
+  passport.use(new DiscordStrategy({
+    clientID: config.discord.clientID,
+    clientSecret: config.discord.clientSecret,
+    callbackURL: config.discord.callbackURL,
+    scope: ['identify', 'email']
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ discordId: profile.id });
+      if (!user) {
+        user = await User.create({
+          discordId: profile.id,
+          username: profile.username,
+          discriminator: profile.discriminator,
+          avatar: profile.avatar,
+          email: profile.email || ''
+        });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err, null);
+    }
+  }));
+};
