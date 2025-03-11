0af4e47e5574d2ca390709e23caded29c4766b78
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:57:33 -0400

    Update Arcade.js

--- frontend/src/components/Arcade.js
+++ frontend/src/components/Arcade.js
@@ -1,1 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
+const Arcade = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/games')
+      .then(res => setGames(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div className="arcade">
+      <h2>Arcade Games</h2>
+      <ul>
+        {games.map(game => (
+          <li key={game.id}>
+            <h3>{game.name}</h3>
+            <p>{game.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Arcade;
