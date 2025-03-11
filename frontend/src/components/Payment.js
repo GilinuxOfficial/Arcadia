7c35b0e221703760813678ed35794481206fd255
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:58:46 -0400

    Update Payment.js

--- frontend/src/components/Payment.js
+++ frontend/src/components/Payment.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+const Payment = () => {
+  return (
+    <div className="payment">
+      <h2>Payment Portal</h2>
+      <p>Securely purchase currencies via PayPal or Bitcoin.</p>
+      {/* Payment integration logic can be added here */}
+    </div>
+  );
+};
+
+export default Payment;
