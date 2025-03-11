eab349932b04519efc40215333ac9082fee00d95
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:56:41 -0400

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Deviant Lounge</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/arcade">Arcade</Link></li>
+        <li><Link to="/club">The Inferno Club</Link></li>
+        <li><Link to="/chat">The Inferno Lounge</Link></li>
+        <li><Link to="/leaderboard">Leaderboard</Link></li>
+        <li><Link to="/payment">Payment</Link></li>
+        <li><Link to="/analytics">Analytics</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
