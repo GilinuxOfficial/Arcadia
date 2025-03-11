220aff90f13555c079c8a7acf0351e3e70350c76
GilinuxOfficial <134341677+GilinuxOfficial@users.noreply.github.com>
2025-03-10 22:54:57 -0400

    Update Achievement.js

--- backend/models/Achievement.js
+++ backend/models/Achievement.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const AchievementSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  criteria: { type: String }
+});
+
+module.exports = mongoose.model('Achievement', AchievementSchema);
